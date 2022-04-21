3f0fc869d81aa8ff8efb55f7ca81b70cfff2120d
Samuel-UC <68044712+Samuel-UC@users.noreply.github.com>
2022-04-20 18:46:50 -0700

    Option to specify directory for large image sequences

--- main.py
+++ main.py
@@ -4,20 +4,50 @@ from pathlib import Path
 # Get arguments
 files = sys.argv[1:]
 
-
-# Verify all arguments are valid files
 valid_files = []
 
-for file in files:
-    path = Path(file)
 
-    if path and path.is_file():
-        valid_files.append(path)
+if len(files) > 1:
+    for file in files:
+        path = Path(file)
 
+        if path and path.is_file():
+            valid_files.append(path)
+else:
+    
+    directory = ""
+    valid_dir = False
+    
+    while not valid_dir:
+        
+        directory = input("Enter directory: ")
+        if os.path.isdir(directory):
+            
+            directory = Path(directory)
+            files_tmp = []
+            
+            for file in directory.iterdir():
+                if file.is_file():
+                    files_tmp.append(file)
+
+            print("\nFound files:", len(files_tmp))
+            print("Preview (3)", files_tmp[:3])
+
+            if input("\nWould you like to use these files? (y/n) ") == "y":
+                valid_dir = True
+                valid_files = files_tmp
+                
 
 # Sort the files based on name
 def getValue(value):
     name = value.stem
+
+    try:
+        name = int(name)
+    except Exception as err:
+        print("Invalid name:", name)
+        return name
+    
     return int(name)
 
 # Options to reverse because Blender sometimes likes it backwards
